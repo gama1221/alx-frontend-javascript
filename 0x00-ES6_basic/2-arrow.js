0bd9fda83b21995dc6d12b27a680e2749baec2f9
getinet amare <getinetamare@gmail.com>
2023-06-26 16:15:32 +0300

    Add arrow functions

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+export default function getNeighborhoodsList() {
+     /* eslint-disable */
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+    
+    const self = this;
+    this.addNeighborhood = (newNeighborhood)=> {
+      self.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return self.sanFranciscoNeighborhoods;
+    };
+  }
