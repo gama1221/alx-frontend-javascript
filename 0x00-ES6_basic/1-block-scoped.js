5ba19c67217de5ece1ec1580130805410142a574
getinet amare <getinetamare@gmail.com>
2023-06-26 16:09:58 +0300

    Modify 1-block-scoped.js file

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
-/* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    /* eslint-disable */
+    task;
+    task2;
   }
+  
+  return [task, task2];
+}
