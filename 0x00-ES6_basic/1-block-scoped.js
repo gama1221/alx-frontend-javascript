3c71d3aa193018c34b2a69730ae1225c3f9286f2
getinet amare <getinetamare@gmail.com>
2023-06-26 15:06:11 +0300

    Add Block Scope file

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+    var task = false;
+    var task2 = true;
+  
+    if (trueOrFalse) {
+        task;
+        task2;
+    }
+  
+    return [task, task2];
+  }
