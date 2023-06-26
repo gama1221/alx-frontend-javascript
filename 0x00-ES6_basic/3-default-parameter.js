92b28d2682920cdc910fcaf712109eb84d73797c
getinet amare <getinetamare@gmail.com>
2023-06-26 16:41:57 +0300

    Modify 3-default-parameter.js file

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,4 +1,4 @@
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-  /* eslint-disable */
-  return initialNumber + expansion1989 + expansion2019;
+    /* eslint-disable */
+   return initialNumber + expansion1989 + expansion2019;
 } 
