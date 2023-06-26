d28857e740fdded2ee7d08056856f002ffa7caf5
getinet amare <getinetamare@gmail.com>
2023-06-26 18:38:58 +0300

    ES6 Method properties fixed

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -3,8 +3,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: (income) =>`$${income}`,
-    getIncomeInEuros: (income) =>`${income} euros`,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;
