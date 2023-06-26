0b5d782f2a975da0a42182bfa8e91e58466a63ee
getinet amare <getinetamare@gmail.com>
2023-06-26 16:20:00 +0300

    Modify 2-arrow.js file

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,10 @@
 export default function getNeighborhoodsList() {
-     /* eslint-disable */
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-    
-    const self = this;
-    this.addNeighborhood = (newNeighborhood) => {
-      self.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return self.sanFranciscoNeighborhoods;
-    };
-  }
+  /* eslint-disable */
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
