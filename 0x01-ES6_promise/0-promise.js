9ade91e56d9de95ff1950e92fc84433afe6c58ff
getinet amare <getinetamare@gmail.com>
2023-06-27 18:04:29 +0300

    Modify 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,9 @@
 export default function getResponseFromAPI(){
-  return new Promise(resolve=>{
-    resolve();
+  return new Promise((resolve, reject)=>{
+    if(resolve){      
+      resolve("success");
+    }else{
+      reject("Error");
+    }
   });
 }
