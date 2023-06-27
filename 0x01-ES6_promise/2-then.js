9d42a6ab6fce3adb6eedb802890ff7d8f6995830
getinet amare <getinetamare@gmail.com>
2023-06-27 16:57:49 +0300

    Create 2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+function handleResponseFromAPI(promise){
+  return (Promise.resolve(promise))?(
+    console.log('Got a response from the API'),
+    {status: 200,body: 'success'}
+  ):(
+    console.log('Got a response from the API'),
+    Error
+  )
+}
