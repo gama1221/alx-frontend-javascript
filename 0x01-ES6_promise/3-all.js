db0b050a859c21ab9a66bc04998872d367059b70
getinet amare <getinetamare@gmail.com>
2023-06-28 12:49:08 +0300

    Fix indentation

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
+  /* eslint-disable */
   return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
       console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
