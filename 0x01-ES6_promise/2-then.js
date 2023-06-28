75fca7da79364b3a7cdce250485ef69021539188
getinet amare <getinetamare@gmail.com>
2023-06-28 12:22:17 +0300

    Modify 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,7 @@
 export default function handleResponseFromAPI(promise){
   /* eslint-disable */
-  return (Promise.resolve(promise))?(
-    console.log('Got a response from the API'),
-    {status: 200,body: 'success'}
-  ):(
-    console.log(Error('Got a response from the API'))
-  )
+  return promise
+  .then(()=>({status: 200,body: 'success'}))
+  .catch(()=>new Error())
+  .finally(()=>console.log('Got a response from the API'))
 }
