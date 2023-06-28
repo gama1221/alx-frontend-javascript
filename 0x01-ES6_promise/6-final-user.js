5e65865dfa8f452dd8b6a4cf60070fccf5f2d10e
getinet amare <getinetamare@gmail.com>
2023-06-28 13:53:30 +0300

    Create 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,6 +3,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
+  /* eslint-disable */
   const user = {
     status: 'pending',
     value: '',
