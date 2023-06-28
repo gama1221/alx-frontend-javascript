ec4d0b29c76de81ee6058248591a50a71d53e579
getinet amare <getinetamare@gmail.com>
2023-06-28 12:42:01 +0300

    Modify 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,9 +4,9 @@ export default function handleProfileSignup(){
 	/* eslint-disable */
 	return (
 		Promise.all([uploadPhoto(), createUser()])
-		.then((data) =>{
+		.then((data) => {
 		console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
 		})
-		.catch(() =>console.log('Signup system offline'))
+		.catch(() => console.log('Signup system offline'))
 	);
 }
