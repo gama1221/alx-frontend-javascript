3e75d7ba78882b61cd5335df4c9c0e0fd3ca2722
getinet amare <getinetamare@gmail.com>
2023-06-28 12:48:00 +0300

    Fix indentation

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import {uploadPhoto, createUser} from './utils';
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup(){
-	/* eslint-disable */
-	return Promise.all([uploadPhoto(), createUser()])
-  	.then((data) => {
-		console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
-		})
-		.catch(() => console.log('Signup system offline'));	
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    })
+
+    .catch(() => console.log('Signup system offline'));
 }
