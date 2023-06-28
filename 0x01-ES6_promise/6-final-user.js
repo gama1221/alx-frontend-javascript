8371221e469a19396acbb08d7b1f6f3db0f82e06
getinet amare <getinetamare@gmail.com>
2023-06-28 13:47:09 +0300

    Create 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  /* eslint-disable */
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((values) => {
     const arr = [];
     for (const item of values) {
