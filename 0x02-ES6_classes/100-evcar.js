9f93ce68cc4fbf96c4ac0d54429969dd9ea4cca3
getinet amare <getinetamare@gmail.com>
2023-06-29 18:09:33 +0300

    100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,15 @@
+import Car from './10-car';
+
+class EVCar extends Car {
+  /* eslint-disable */
+  constructor(brand = '', motor = '', color = '', range = '') {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+}
+
+export default EVCar;
