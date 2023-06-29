385cb0e1a3b1c8a7c4efd8d40eb829113db4ca11
getinet amare <getinetamare@gmail.com>
2023-06-29 15:06:10 +0300

    Create 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -12,40 +12,40 @@ export default class HolbertonCourse{
     }else{
       throw TypeError("Length must be a number");
     }
-    if(!Array.isArray(students) && students.every((item) => typeof i !== 'string')){
+    if(!Array.isArray(students) && students.every((item) => typeof item !== 'string')){
       throw TypeError('Students must be a string of arrays');
     }else{
       this._students = students;
     }
   }
-  setName(name){
+  set name(name){
     if(typeof name !== 'string'){
       throw TypeError('Name must be a String');
     }else{
       this._name = name;      
     }
   }
-  getName(){
+  get name(){
     return this._name;
   }
-  setLength(length){
+  set length(length){
     if(typeof length !== 'number'){
       throw TypeError("Length must be a number");
     }else{
       this._length = length;
     }    
   }
-  getLength(){
+  get length(){
     return this._length;
   }
-  setStudents(students){
-    if(!Array.isArray(students) && students.every((item) => typeof i !== 'string')){
+  set students(students){
+    if(!Array.isArray(students) && students.every((item) => typeof item !== 'string')){
       throw TypeError('Students must be a string of arrays');
     }else{
       this._students = students;
     }
   }
-  getStudents(){
+  get students(){
     return this._students;
   }
 }
