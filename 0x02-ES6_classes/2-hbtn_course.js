591949c00ebb7d6f959c28a7832a4c07ce6d0223
getinet amare <getinetamare@gmail.com>
2023-06-29 15:09:22 +0300

    Fix 2-hbtn_course.js lint

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,47 +1,47 @@
 
-export default class HolbertonCourse{
+export default class HolbertonCourse {
   /* eslint-disable */
-  constructor(name, length, students){
-    if(typeof name === 'string'){
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
       this._name = name;
-    }else{
+    } else {
       throw TypeError('Name must be a String');
     }
-    if(typeof length === 'number'){
+    if (typeof length === 'number') {
       this._length = length;
-    }else{
+    } else {
       throw TypeError("Length must be a number");
     }
-    if(!Array.isArray(students) && students.every((item) => typeof item !== 'string')){
+    if (!Array.isArray(students) && students.every((item) => typeof item !== 'string')) {
       throw TypeError('Students must be a string of arrays');
-    }else{
+    } else {
       this._students = students;
     }
   }
-  set name(name){
-    if(typeof name !== 'string'){
+  set name(name) {
+    if (typeof name !== 'string') {
       throw TypeError('Name must be a String');
-    }else{
+    } else {
       this._name = name;      
     }
   }
-  get name(){
+  get name() {
     return this._name;
   }
-  set length(length){
-    if(typeof length !== 'number'){
+  set length(length) {
+    if (typeof length !== 'number') {
       throw TypeError("Length must be a number");
-    }else{
+    } else {
       this._length = length;
     }    
   }
-  get length(){
+  get length() {
     return this._length;
   }
-  set students(students){
-    if(!Array.isArray(students) && students.every((item) => typeof item !== 'string')){
+  set students(students) {
+    if (!Array.isArray(students) && students.every((item) => typeof item !== 'string')) {
       throw TypeError('Students must be a string of arrays');
-    }else{
+    } else {
       this._students = students;
     }
   }
