49339a0f5350940c8ba734b6da37080da7fca57d
Getinet <getinetamare@gmail.com>
2023-07-03 19:00:36 +0300

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,9 @@
-const getListStudents = () => [
+const getListStudents = () => {
+  /* eslint-disable */
+  [
   { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
   { id: 2, firstName: 'James', location: 'Columbia' },
   { id: 5, firstName: 'Serena', location: 'San Francisco' },
-]
-/* eslint-disable */
+]}
+
 export default getListStudents;
