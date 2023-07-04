54c5c97240fcaf3b127ee27e59a7e1868f95296b
getinet amare <getinetamare@gmail.com>
2023-07-04 08:56:28 +0300

    Fix based on linter rule

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,6 @@
-const getStudentIdsSum = (students) => {
+export default function getStudentIdsSum(students) {
+  /* eslint-disable */
   const initailValue = 0;
   const studentIdsSum = students.reduce((accomulator, currentValue) => accomulator + currentValue.id, initailValue);
   return studentIdsSum;
 }
-/* eslint-disable */
-export default getStudentIdsSum;
