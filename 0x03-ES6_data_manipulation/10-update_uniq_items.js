133f57fd9286bf4ded6313143f7d575825a48446
getinet amare <getinetamare@gmail.com>
2023-07-04 09:13:11 +0300

    Create 10-update_uniq_items.js file

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+export default function updateUniqueItems(map) {
+  /* eslint-disable */
+  if (!(map instanceof Map)) {
+    throw Error('Cannot process');
+  }
+  for (const elements of map) {
+    if (elements[1] === 1) {
+      map.set(elements[0], 100);
+    }
+  }
+  return map;
+}
+  
