2eb51155d5c7ae6558b9aaff013da8ac3422e656
getinet amare <getinetamare@gmail.com>
2023-07-04 09:09:16 +0300

    Create 8_clean_set.js file

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  /* eslint-disable */
+  let res = '';
+  if (!startString || !startString.length) {
+    return res;
+  }
+  set.forEach((element) => {
+    if (element && element.startsWith(startString)) {
+      res += `${element.slice(startString.length)}-`;
+    }
+  });
+  return res.slice(0, res.length - 1);
+}
