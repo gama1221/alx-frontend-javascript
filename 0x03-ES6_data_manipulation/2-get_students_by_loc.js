1f774a0e0112f45cc36dbc4cd85b413bae501c16
getinet amare <getinetamare@gmail.com>
2023-07-04 08:51:50 +0300

    Fix indentation problem

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,8 +1,6 @@
-const getStudentsByLocation  = (students, location)=>{
-    let studentsByLocation = [];
-    studentsByLocation = students.filter(student=>student.location === location);
-    return studentsByLocation;
+export default function getStudentsByLocation(students, location) {
+  /* eslint-disable */
+  let studentsByLocation = [];
+  studentsByLocation = students.filter(student => student.location === location);
+  return studentsByLocation;
 }
-
-/* eslint-disable */
-export default getStudentsByLocation;
