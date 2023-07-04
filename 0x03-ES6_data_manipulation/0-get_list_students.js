23297406512edcbd295e08b60b11c00d5b8a1de6
getinet amare <getinetamare@gmail.com>
2023-07-04 08:40:22 +0300

    Fix indentation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,8 @@
-/* eslint-disable */
-const getListStudents = () => {
+function getListStudents() {
+  /* eslint-disable */
   [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ]
 }
-
-export default getListStudents;
