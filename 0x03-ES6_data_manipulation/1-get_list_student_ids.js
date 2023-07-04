231d07623999a33d3e94d8bb5e046baf47ca125f
getinet amare <getinetamare@gmail.com>
2023-07-04 08:52:55 +0300

    Fix based on linter rule

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,9 @@
 export default function getListStudentIds(listStudents) {
-    /* eslint-disable */
-    let listStudentIds = [];
-    if (!(listStudents instanceof Array)) {
-        return listStudentIds;
-    }
-    listStudentIds = listStudents.map(student => student.id);
-    return listStudentIds;
+  /* eslint-disable */
+  let listStudentIds = [];
+  if (!(listStudents instanceof Array)) {
+      return listStudentIds;
+  }
+  listStudentIds = listStudents.map(student => student.id);
+  return listStudentIds;
 }
